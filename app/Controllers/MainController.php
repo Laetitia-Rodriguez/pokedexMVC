872d7fe1ad575f6b842e910444af10f11a5bdbc4
LaetitiaRodriguez <laetirodriguez@hotmail.com>
2021-05-28 17:59:11 +0200

    routes and maincontroller function show

--- app/Controllers/MainController.php
+++ app/Controllers/MainController.php
@@ -1,2 +1,23 @@
 <?php 
-essai
+
+class MainController {
+
+    // function to show the homepage
+    public function home() {
+        $this->show('home');
+    }
+
+
+    //function to show details pages
+    public function detail() {
+        $this->show('detail');
+    }
+
+    // function to show the views
+    private function show($viewName, $viewVars=[]) {
+        // templates header and footer and page which is in parameter
+        include(__DIR__.'/../views/header.tpl.php');
+        include(__DIR__.'/../views/' . $viewName . '.tpl.php');
+        include(__DIR__.'/../views/footer.tpl.php');
+    }
+}
--- public/index.php
+++ public/index.php
@@ -1,1 +1,20 @@
 <?php
+
+// Composer dependencies
+require __DIR__.'/..vendor/autoload.php';
+
+// Altorouter is a composer dependency for router
+$router = new AltoRouter();
+
+// Basepath with BASE_URI is determined in .htaccess
+$router->setBasePath($_SERVER['BASE_URI']);
+
+// Routes
+$router->map('GET', '/', 'MainController::home');
+$router->map('GET', '/detail', 'MainController::detail');
+
+$match = $router->match();
+
+if ($match!==false) {
+    echo 'Page non trouvée';
+}
